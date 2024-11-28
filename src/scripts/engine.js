f5b7b5c706365ce533834315f746b28e2518bfff
Nicollas <nicollasdejorge@gmail.com>
2024-11-28 18:03:10 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -268,6 +268,7 @@ function init() {
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
     const bgm = document.getElementById("bgm");
+    bgm.volume = 0.5;
     bgm.play();
 }
 
